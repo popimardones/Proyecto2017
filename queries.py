0e708ade9e660acce198dba9d046b86cb39d512d
rodrigomartinezar <30805946+rodrigomartinezar@users.noreply.github.com>
2017-11-26 13:21:41 -0300

    Queries con más información

--- queries.py
+++ queries.py
@@ -15,16 +15,22 @@ client = MongoClient('localhost', 27017)
 candidatoId = ['1481491872064849', '553775568008058', '10152723078', '377671865775887', '137510593443379', '881095048648989', '321406001578434', '386634201382499'] 
 #candidatoId ordenado segun la lista "candidatos"
 posicionId = 0
+aprobaciones= []
+aprobacionesNumero= []
+candidatosPubliNoVacias= []
+candidatosPubliVacias= []
 meses = ['Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre']
 for candidato in candidatos:
+	publiVacias=0
+	publiNoVacias=0
 	totalComentarios = 0
 	totalPositivos = 0
+	IdCandidatoActual = candidatoId[posicionId]
+	database = client[candidato+"JSONcomments"]
 	for mes in meses:
+		sys.stdout = io.TextIOWrapper(sys.stdout.detach(), sys.stdout.encoding, 'backslashreplace')
 		positivosEnMes = 0
 		comentariosEnMes = 0
-		IdCandidatoActual = candidatoId[posicionId]
-		database = client[candidato+"JSONcomments"]
-		sys.stdout = io.TextIOWrapper(sys.stdout.detach(), sys.stdout.encoding, 'backslashreplace')
 		fileRead = open(candidato+'_postIds_'+mes+'.txt', 'r')
 		presidente_txtList = fileRead.readlines()
 		# saca el \n de todos los item de la lista
@@ -48,20 +54,43 @@ for candidato in candidatos:
 			        comments.append(doc["message"])
 			finally:
 			    cursor.close()
-		lista = cm.predict(comments, params)
+		if len(comments) > 0:
+			publiNoVacias = publiNoVacias + 1
+			lista = cm.predict(comments, params)
+		else:
+			publiVacias = publiVacias + 1
 		#print(comments)
 		comentariosEnMes = len(comments)
 		print(candidato)
 		print(mes)
-		print(comentariosEnMes)
+		print('Cantidad de publicaciones: {}'.format(len(presidenteSoloPostId_List)))
+		print('Comentarios en publicacion: {}'.format(comentariosEnMes))
+		promedioMes= (comentariosEnMes/len(presidenteSoloPostId_List))
+		print('Promedio de comentarios por publicacion: {}'.format(promedioMes))
 		for index in range(0, len(lista)):
 			positivosEnMes += lista[index]
 		porcentajePositivoEnMes = ((positivosEnMes / comentariosEnMes) * 100)
 		print("{0:0.2f}% aprobación".format(porcentajePositivoEnMes))
+		print("\n")
 		totalComentarios += comentariosEnMes
 		totalPositivos += positivosEnMes
-		posicionId +=1
+	posicionId +=1
+	print(candidato)
 	print('Total comentarios: ')
 	print(totalComentarios)
 	porcentajePositivoTotal = ((totalPositivos/ totalComentarios)*100)
 	print("{0:0.2f}% aprobación".format(porcentajePositivoTotal))
+	print("\n")
+	print("cambio de candidato")
+	#Listas con aprobaciones, publicaciones no vacias y vacias
+	aprobaciones.append(porcentajePositivoTotal)
+	aprobacionesNumero.append(totalPositivos)
+	candidatosPubliNoVacias.append(publiNoVacias)
+	candidatosPubliVacias.append(publiVacias)
+print()
+print("PORCENTAJE DE POSITIVIDAD DE LOS COMENTARIOS DE FACEBOOK DE LAS PUBLICACIONES DE LOS CANDIDATOS:\n")
+for i in range(len(candidatos)):
+    print(candidatos[i]+ ': {0:0.2f}% - {1} Comentarios positivos'.format(aprobaciones[i], aprobacionesNumero[i]))
+    #print('  Publicaciones con comentarios: {}'.format(candidatosPubliNoVacias[i]))
+    #print('  Publicaciones vacias: {}'.format(candidatosPubliVacias[i]))
+    print()
